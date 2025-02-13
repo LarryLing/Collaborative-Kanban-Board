29cefbb4e422a3a9878b22d88f8144b5d8384e69
CS376-Freestyle-Game <larryling.main@gmail.com>
2025-02-12 18:40:28 -0600

    Update boards display header

--- src/components/blocks/dashboard/boards-display-header.tsx
+++ src/components/blocks/dashboard/boards-display-header.tsx
@@ -1,39 +1,193 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group"
-import { ArrowDownAZ, Bookmark, List, Plus } from "lucide-react"
-import React from "react"
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Toggle } from "@/components/ui/toggle"
+import {
+	ArrowDownAZ,
+	Bookmark,
+	Check,
+	LayoutGrid,
+	List,
+	Plus,
+} from "lucide-react"
+import React, { useState } from "react"
+
+type OwnershipTypes = "Owned by anyone" | "Owned by me" | "Not owned by me"
+type SortMethodTypes = "Last Opened" | "Sort Ascending" | "Sort Descending"
 
 export default function BoardsDisplayHeader() {
+	const [ownership, setOwnership] =
+		useState<OwnershipTypes>("Owned by anyone")
+	const [listView, setListView] = useState(false)
+	const [bookmarked, setBookmarked] = useState(false)
+	const [sortMethod, setSortMethod] = useState<SortMethodTypes>("Last Opened")
+
 	return (
 		<div className="w-full flex flex-col gap-4">
 			<h2 className="font-semibold text-3xl">My Boards</h2>
 			<div className="flex justify-between">
 				<Button>
-					<Plus className="h-4 w-4" /> New Board
+					<Plus className="size-4" /> New Board
 				</Button>
-				<ToggleGroup type="multiple" variant="outline">
-					<ToggleGroupItem
-						value="ownership"
-						aria-label="Toggle ownership"
-					>
-						Owned by anyone
-					</ToggleGroupItem>
-					<ToggleGroupItem value="view" aria-label="Toggle view">
-						<List className="h-4 w-4" />
-					</ToggleGroupItem>
-					<ToggleGroupItem
-						value="bookmarked"
-						aria-label="Toggle strikethrough"
-					>
-						<Bookmark className="h-4 w-4" />
-					</ToggleGroupItem>
-					<ToggleGroupItem value="sort" aria-label="Toggle sort">
-						<ArrowDownAZ className="h-4 w-4" />
-					</ToggleGroupItem>
-				</ToggleGroup>
+				<div className="flex gap-2">
+					<OwnershipDropdown
+						ownership={ownership}
+						setOwnership={setOwnership}
+					/>
+					<ViewToggle listView={listView} setListView={setListView} />
+					<BookmarkToggle
+						bookmarked={bookmarked}
+						setBookmarked={setBookmarked}
+					/>
+					<SortDropdown
+						sortMethod={sortMethod}
+						setSortMethod={setSortMethod}
+					/>
+				</div>
 			</div>
 		</div>
+	)
+}
+
+type OwnershipDropdownProps = {
+	ownership: OwnershipTypes
+	setOwnership: (arg0: OwnershipTypes) => void
+}
+
+function OwnershipDropdown({
+	ownership,
+	setOwnership,
+}: OwnershipDropdownProps) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline" className="w-[160px]">
+					{ownership}
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuItem
+					onSelect={() => setOwnership("Owned by anyone")}
+				>
+					{ownership === "Owned by anyone" ? (
+						<Check className="size-4" />
+					) : (
+						<div className="size-4"></div>
+					)}
+					<span>Owned by anyone</span>
+				</DropdownMenuItem>
+				<DropdownMenuItem onSelect={() => setOwnership("Owned by me")}>
+					{ownership === "Owned by me" ? (
+						<Check className="size-4" />
+					) : (
+						<div className="size-4"></div>
+					)}
+					<span>Owned by me</span>
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					onSelect={() => setOwnership("Not owned by me")}
+				>
+					{ownership === "Not owned by me" ? (
+						<Check className="size-4" />
+					) : (
+						<div className="size-4"></div>
+					)}
+					<span>Not owned by me</span>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
+
+type ViewToggleProps = {
+	listView: boolean
+	setListView: (arg0: boolean) => void
+}
+
+function ViewToggle({ listView, setListView }: ViewToggleProps) {
+	return (
+		<Toggle
+			aria-label="Toggle view"
+			variant="outline"
+			pressed={listView}
+			onPressedChange={() => setListView(!listView)}
+		>
+			{listView ? (
+				<List className="size-4" />
+			) : (
+				<LayoutGrid className="size-4" />
+			)}
+		</Toggle>
+	)
+}
+
+type BookmarkToggleProps = {
+	bookmarked: boolean
+	setBookmarked: (arg0: boolean) => void
+}
+
+function BookmarkToggle({ bookmarked, setBookmarked }: BookmarkToggleProps) {
+	return (
+		<Toggle
+			aria-label="Toggle view"
+			variant="outline"
+			pressed={bookmarked}
+			onPressedChange={() => setBookmarked(!bookmarked)}
+		>
+			<Bookmark className="size-4" />
+		</Toggle>
+	)
+}
+
+type SortDropdownProps = {
+	sortMethod: SortMethodTypes
+	setSortMethod: (arg0: SortMethodTypes) => void
+}
+
+function SortDropdown({ sortMethod, setSortMethod }: SortDropdownProps) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="outline" className="px-2 py-0">
+					<ArrowDownAZ className="size-4" />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuItem onSelect={() => setSortMethod("Last Opened")}>
+					{sortMethod === "Last Opened" ? (
+						<Check className="size-4" />
+					) : (
+						<div className="size-4"></div>
+					)}
+					<span>Last Opened</span>
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					onSelect={() => setSortMethod("Sort Ascending")}
+				>
+					{sortMethod === "Sort Ascending" ? (
+						<Check className="size-4" />
+					) : (
+						<div className="size-4"></div>
+					)}
+					<span>Sort Ascending</span>
+				</DropdownMenuItem>
+				<DropdownMenuItem
+					onSelect={() => setSortMethod("Sort Descending")}
+				>
+					{sortMethod === "Sort Descending" ? (
+						<Check className="size-4" />
+					) : (
+						<div className="size-4"></div>
+					)}
+					<span>Sort Descending</span>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
 	)
 }
