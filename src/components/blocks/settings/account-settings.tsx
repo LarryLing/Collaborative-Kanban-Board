340ecb43e5d31c1f5e1a7e388745b236a8e7ffaf
CS376-Freestyle-Game <larryling.main@gmail.com>
2025-03-01 17:50:47 -0600

    Add dialogs to account settings page

--- src/components/blocks/settings/account-settings.tsx
+++ src/components/blocks/settings/account-settings.tsx
@@ -9,96 +9,259 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
-import React from "react";
+import React, { useState } from "react";
 import { UserProfile } from "@/lib/types";
+import { Input } from "@/components/ui/input";
 
 type AccountSettingsProps = {
 	userProfile: UserProfile;
 };
 
 export default function AccountSettings({ userProfile }: AccountSettingsProps) {
+	const [isAccountDeleteDialogOpen, setIsAccountDeleteDialogOpen] =
+		useState(false);
+	const [isUpdateEmailDialogOpen, setIsUpdateEmailDialogOpen] =
+		useState(false);
+	const [isUpdatePasswordDialogOpen, setIsUpdatePasswordIsDialogOpen] =
+		useState(false);
+
 	return (
-		<Card className="border-none shadow-none">
-			<CardHeader className="pt-0 pr-0">
-				<CardTitle>Account</CardTitle>
-				<CardDescription>
-					Set your preferred language and manage your account
-					credentials.
-				</CardDescription>
-			</CardHeader>
-			<CardContent className="space-y-6 pr-0">
-				<Separator className="w-full" />
-				<div className="space-y-1">
-					<Label>Email</Label>
-					<p className="text-sm text-muted-foreground font-normal">
-						This is the email you will use to login and receive
-						emails for.
-					</p>
-					<Button variant="outline" className="block">
+		<>
+			<Card className="border-none shadow-none">
+				<CardHeader className="pt-0 pr-0">
+					<CardTitle>Account</CardTitle>
+					<CardDescription>
+						Set your preferred language and manage your account
+						credentials.
+					</CardDescription>
+				</CardHeader>
+				<CardContent className="space-y-6 pr-0">
+					<Separator className="w-full" />
+					<div className="space-y-1">
+						<Label>Email</Label>
+						<p className="text-sm text-muted-foreground font-normal">
+							This is the email you will use to login and receive
+							emails for.
+						</p>
+						<Button
+							variant="outline"
+							onClick={() => setIsUpdateEmailDialogOpen(true)}
+						>
+							Update Email
+						</Button>
+					</div>
+					<div className="space-y-1">
+						<Label>Password</Label>
+						<p className="text-sm text-muted-foreground font-normal">
+							This will be the password you use for future logins.
+						</p>
+						<Button
+							variant="outline"
+							onClick={() =>
+								setIsUpdatePasswordIsDialogOpen(true)
+							}
+						>
+							Update Password
+						</Button>
+					</div>
+					<div className="space-y-1">
+						<Label>Delete Account</Label>
+						<p className="text-sm text-muted-foreground font-normal">
+							This action cannot be undone. Please be certain
+						</p>
+						<Button
+							variant="destructive"
+							onClick={() => setIsAccountDeleteDialogOpen(true)}
+						>
+							Delete Account
+						</Button>
+					</div>
+				</CardContent>
+			</Card>
+			<DeleteAccountDialog
+				isDialogOpen={isAccountDeleteDialogOpen}
+				setIsDialogOpen={setIsAccountDeleteDialogOpen}
+				displayname={userProfile.display_name}
+			/>
+			<UpdateEmailDialog
+				isDialogOpen={isUpdateEmailDialogOpen}
+				setIsDialogOpen={setIsUpdateEmailDialogOpen}
+				email={userProfile.email}
+			/>
+			<UpdatePasswordDialog
+				isDialogOpen={isUpdatePasswordDialogOpen}
+				setIsDialogOpen={setIsUpdatePasswordIsDialogOpen}
+			/>
+		</>
+	);
+}
+
+type DialogProps = {
+	isDialogOpen: boolean;
+	setIsDialogOpen: (arg0: boolean) => void;
+};
+
+type DeleteAccountDialogProps = {
+	displayname: string;
+};
+
+function DeleteAccountDialog({
+	isDialogOpen,
+	setIsDialogOpen,
+	displayname,
+}: DialogProps & DeleteAccountDialogProps) {
+	return (
+		<Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Delete Account</DialogTitle>
+					<DialogDescription>
+						Please type your username below to confirm this action.
+					</DialogDescription>
+				</DialogHeader>
+				<form>
+					<Input
+						id="delete"
+						name="delete"
+						type="text"
+						placeholder={displayname}
+						className="max-w-[400px]"
+					/>
+				</form>
+				<DialogFooter>
+					<Button
+						variant="outline"
+						onClick={() => setIsDialogOpen(false)}
+					>
+						Go back
+					</Button>
+					<Button
+						type="submit"
+						variant="destructive"
+						className="mb-2 sm:mb-0"
+					>
+						Delete Account
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+type UpdateEmailDialogProps = {
+	email: string;
+};
+
+function UpdateEmailDialog({
+	isDialogOpen,
+	setIsDialogOpen,
+	email,
+}: DialogProps & UpdateEmailDialogProps) {
+	return (
+		<Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Update Email</DialogTitle>
+					<DialogDescription>
+						Enter your new email below. Confirmation emails will be
+						sent to your old and new inboxes.
+					</DialogDescription>
+				</DialogHeader>
+				<form>
+					<Input
+						id="delete"
+						name="delete"
+						type="text"
+						defaultValue={email}
+						className="max-w-[400px]"
+					/>
+				</form>
+				<DialogFooter>
+					<Button
+						variant="outline"
+						onClick={() => setIsDialogOpen(false)}
+					>
+						Go back
+					</Button>
+					<Button type="submit" className="mb-2 sm:mb-0">
 						Update Email
 					</Button>
-				</div>
-				<div className="space-y-1">
-					<Label>Password</Label>
-					<p className="text-sm text-muted-foreground font-normal">
-						This will be the password you use for future logins.
-					</p>
-					<Button variant="outline" className="block">
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+function UpdatePasswordDialog({ isDialogOpen, setIsDialogOpen }: DialogProps) {
+	return (
+		<Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Confirm Delete</DialogTitle>
+					<DialogDescription>
+						Are you sure you want to delete this board?
+					</DialogDescription>
+				</DialogHeader>
+				<form className="flex flex-col justify-center items-start gap-2">
+					<Input
+						id="currentPassword"
+						name="currentPassword"
+						type="password"
+						placeholder="Current Password"
+						className="max-w-[400px]"
+					/>
+					{/* {state?.errors?.password && (
+									<p className="text-sm text-destructive">
+										{state.errors.password}
+									</p>
+								)} */}
+					<Input
+						id="newPassword"
+						name="newPassword"
+						type="password"
+						placeholder="New Password"
+						className="max-w-[400px]"
+					/>
+					{/* {state?.errors?.newPassword && (
+									<p className="text-sm text-destructive">
+										{state.errors.newPassword}
+									</p>
+								)} */}
+					<Input
+						id="confirmPassword"
+						name="confirmPassword"
+						type="password"
+						placeholder="Confirm Password"
+						className="max-w-[400px]"
+					/>
+					{/* {state?.errors?.confirmPassword && (
+									<p className="text-sm text-destructive">
+										{state.errors.confirmPassword}
+									</p>
+								)} */}
+				</form>
+				<DialogFooter>
+					<Button
+						variant="outline"
+						onClick={() => setIsDialogOpen(false)}
+					>
+						Go back
+					</Button>
+					<Button type="submit" className="mb-2 sm:mb-0">
 						Update Password
 					</Button>
-				</div>
-				{/* <div className="space-y-1">
-					<Label htmlFor="social">Password</Label>
-					<div className="flex flex-col justify-center items-start gap-2">
-						<Input
-							id="currentPassword"
-							name="currentPassword"
-							type="text"
-							placeholder="Current Password"
-							className="max-w-[400px]"
-						/>
-						{state?.errors?.password && (
-							<p className="text-sm text-destructive">
-								{state.errors.password}
-							</p>
-						)}
-						<Input
-							id="newPassword"
-							name="newPassword"
-							type="text"
-							placeholder="New Password"
-							className="max-w-[400px]"
-						/>
-						{state?.errors?.newPassword && (
-							<p className="text-sm text-destructive">
-								{state.errors.newPassword}
-							</p>
-						)}
-						<Input
-							id="confirmPassword"
-							name="confirmPassword"
-							type="text"
-							placeholder="Confirm Password"
-							className="max-w-[400px]"
-						/>
-						{state?.errors?.confirmPassword && (
-							<p className="text-sm text-destructive">
-								{state.errors.confirmPassword}
-							</p>
-						)}
-					</div>
-					<p className="text-sm text-muted-foreground font-normal"></p>
-				</div> */}
-				<div className="space-y-1">
-					<Label>Delete Account</Label>
-					<p className="text-sm text-muted-foreground font-normal">
-						This action cannot be undone. Please be certain
-					</p>
-					<Button variant="destructive">Delete Account</Button>
-				</div>
-			</CardContent>
-		</Card>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
 	);
 }
