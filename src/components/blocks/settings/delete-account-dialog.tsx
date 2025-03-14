168dbcd4446c00eaeb1475dd234dced58a975134
CS376-Freestyle-Game <larryling.main@gmail.com>
2025-03-14 10:43:23 -0500

    Refactor dialog code

---
+++ src/components/blocks/settings/delete-account-dialog.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogHeader,
+	DialogContent,
+	DialogTitle,
+	DialogDescription,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { deleteAccount } from "@/lib/actions";
+import { useActionState } from "react";
+
+type DeleteAccountDialogProps = {
+	isDialogOpen: boolean;
+	setIsDialogOpen: (arg0: boolean) => void;
+	displayname: string;
+};
+
+export default function DeleteAccountDialog({
+	isDialogOpen,
+	setIsDialogOpen,
+	displayname,
+}: DeleteAccountDialogProps) {
+	const [state, action, pending] = useActionState(deleteAccount, undefined);
+
+	return (
+		<Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Delete Account</DialogTitle>
+					<DialogDescription>
+						Please type your username below to confirm this action.
+					</DialogDescription>
+				</DialogHeader>
+				<form action={action}>
+					<Input
+						id="displayName"
+						name="displayName"
+						type="text"
+						placeholder={displayname}
+						className="max-w-[400px]"
+					/>
+					{state?.errors?.displayName && (
+						<p className="text-sm text-destructive">
+							{state.errors.displayName}
+						</p>
+					)}
+					<div className="flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2 mt-4">
+						<Button
+							type="submit"
+							variant="destructive"
+							className="mb-2 sm:mb-0"
+							disabled={pending}
+						>
+							{pending ? "Deleting..." : "Delete Account"}
+						</Button>
+					</div>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+}
