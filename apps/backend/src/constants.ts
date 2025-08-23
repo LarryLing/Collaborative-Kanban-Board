a8394c40babee7fe0ad2d06a34ca0578c9fc9714
Larry Ling <larryling.main@gmail.com>
2025-08-23 14:58:57 -0500

    removed unused dndkit packages + updated backend environment variable names

--- apps/backend/src/constants.ts
+++ apps/backend/src/constants.ts
@@ -1,9 +1,9 @@
-if (!process.env.AWS_REGION || !process.env.AWS_ACCESS_KEY || !process.env.AWS_SECRET_ACCESS_KEY) {
+if (!process.env.AWS_REGION || !process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
   throw new Error("Missing AWS environment variables!");
 }
 
 export const AWS_REGION = process.env.AWS_REGION;
-export const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
 if (
